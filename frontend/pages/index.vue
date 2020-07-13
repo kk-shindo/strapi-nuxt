6e94c35331c20147a4bbaa1534f86187691435e3
kk-shindo <kk-shindo@respect-pal.jp>
2020-07-13 18:38:26 +0900

    add frontend

---
+++ frontend/pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div v-cloak class="container">
+    <ul>
+      <li v-for="(article, key) in articles" :key="key">
+        <nuxt-link :to="'/blog/'+article.id">
+          {{ article.Title }}
+        </nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  async asyncData ({ params }) {
+    const { data } = await axios.get('http://localhost:1337/blogs')
+    return { articles: data }
+  }
+}
+</script>
+
+<style>
+[v-cloak] {
+  display: none;
+}
+</style>
